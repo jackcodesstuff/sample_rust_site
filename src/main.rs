347734da63aeecbafd9fe28602d2d0def43df682
jackcodesstuff <bloomjack91@gmail.com>
2025-02-26 17:06:07 -0800

    styled, changed index to html

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket_dyn_templates::Template;
+use rocket::fs::{FileServer, relative};
+use rocket::serde::{json::Json, Serialize};
 use lazy_static::lazy_static;
-use std::collections::HashMap;
 use std::sync::Mutex;
 
 lazy_static! {
     static ref VISITOR_COUNT: Mutex<u32> = Mutex::new(0);
 }
 
-#[get("/")]
-fn index() -> Template {
+#[derive(Serialize)]
+struct Count {
+    count: u32,
+}
+
+#[get("/count")]
+fn count() -> Json<Count> {
     let mut count = VISITOR_COUNT.lock().unwrap();
     *count += 1;
-
-    let count_string = count.to_string(); // Create a longer-lived string
-    let mut context = HashMap::new();
-    context.insert("message", "Hello, world!");
-    context.insert("count", &count_string); // Borrow the string slice
-    Template::render("index", &context)
+    Json(Count { count: *count })
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
-        .attach(Template::fairing())
+        .mount("/", FileServer::from(relative!("static")))
+        .mount("/", routes![count])
 }
